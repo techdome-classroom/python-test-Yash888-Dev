abd6637cf8a403f11da1e0ae6da89a3c85231b70
Yash888-Dev <er.yash.sarwate@gmail.com>
2024-07-26 13:30:11 +0530

    Auto commit on save

--- program2.py
+++ program2.py
@@ -17,7 +17,8 @@ def decode_message( s: str, p: str) -> bool:
         if p[j - 1] == '*':
             dp[0][j] = dp[0][j - 1]
     
-    # Fill the rest of the DP table
+   
+   
      for i in range(1, m + 1):
         for j in range(1, n + 1):
             if p[j - 1] == '*':
