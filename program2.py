49f7594b69417fa2a9bed0cf6ea63128fb2fd68b
Yash888-Dev <er.yash.sarwate@gmail.com>
2024-07-26 13:30:28 +0530

    Auto commit on save

--- program2.py
+++ program2.py
@@ -23,10 +23,11 @@ def decode_message( s: str, p: str) -> bool:
         for j in range(1, n + 1):
             if p[j - 1] == '*':
                 
-                
                 dp[i][j] = dp[i][j - 1] or dp[i - 1][j]
+                
             elif p[j - 1] == '?' or p[j - 1] == s[i - 1]:
-                # '?' matches any single character or exact character match
+                
+                
                 dp[i][j] = dp[i - 1][j - 1]
     
     # The result is whether the entire message matches the entire pattern
