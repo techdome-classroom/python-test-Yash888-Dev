fbed6aa4d27048872ade5fd387f49010e6f77d3b
Yash888-Dev <er.yash.sarwate@gmail.com>
2024-07-26 13:08:54 +0530

    Auto commit on save

--- program2.py
+++ program2.py
@@ -2,7 +2,7 @@ def decode_message( s: str, p: str) -> bool:
 
 # write your code here
     def decode_message(s: str, p: str) -> bool:
-    m, n = len(s), len(p)
+     m, n = len(s), len(p)
     
     # Create a 2D DP table with dimensions (m+1) x (n+1)
     dp = [[False] * (n + 1) for _ in range(m + 1)]
