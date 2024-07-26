5ae88479aabcbf770f4cbf18e5901447289db90a
Yash888-Dev <er.yash.sarwate@gmail.com>
2024-07-26 12:38:13 +0530

    Auto commit on save

--- program1.py
+++ program1.py
@@ -6,20 +6,5 @@ class Solution:
         return 0
 class Solution:
     def getTotalIsles(self, grid: list[list[str]]) -> int:
-        def dfs(i, j):
-            if i < 0 or j < 0 or i >= len(grid) or j >= len(grid[0]) or grid[i][j] != 'L':
-                return
-            grid[i][j] = 'V'  # Mark as visited
-            dfs(i + 1, j)
-            dfs(i - 1, j)
-            dfs(i, j + 1)
-            dfs(i, j - 1)
-
-        count = 0
-        for i in range(len(grid)):
-            for j in range(len(grid[0])):
-                if grid[i][j]   
- == 'L':
-                    count += 1
-                    dfs(i, j)
+        
         return count
